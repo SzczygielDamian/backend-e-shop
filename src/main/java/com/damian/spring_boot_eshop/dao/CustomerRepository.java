605299a3fafe3639d55149c3de0a501b3cc4f60d
DESKTOP-ARLO0UC\Tymek <szczygielek92@gmail.com>
2025-04-06 19:31:31 +0200

    fixed bug multiple customer with the same email

--- src/main/java/com/damian/spring_boot_eshop/dao/CustomerRepository.java
+++ src/main/java/com/damian/spring_boot_eshop/dao/CustomerRepository.java
@@ -4,5 +4,5 @@ import com.damian.spring_boot_eshop.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
-
+    Customer findByEmail(String email);
 }
